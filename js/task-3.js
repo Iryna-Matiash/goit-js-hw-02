417d23dbe92da32b2274b3c72b5e84df15646131
Iryna-Matiash <165329434+Iryna-Matiash@users.noreply.github.com>
2024-06-12 23:15:42 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,16 @@
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
 
-
-// function getElementWidth(content, padding, border) {
-//   const paddingValue = Number.parseFloat(padding);
-//   const borderValue = Number.parseFloat(border);
-//   const contentValue = Number.parseFloat(content);
-//   const width = contentValue + (2 * paddingValue) + (2 * borderValue);
-//   return width;
-// }
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-function getElementWidth(content, padding, border){
-    return Number.parseFloat(content) + (Number.parseFloat(padding) * 2) + (Number.parseFloat(border) * 2);
+    if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+        return true;
+    } else {
+        return false;
+    }
 }
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
